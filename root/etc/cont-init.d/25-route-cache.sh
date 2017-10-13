acc23e8b327709e948f380cb96b0e3e3fc3325b0
Luca Critelli <lucacri@gmail.com>
2017-10-13 15:35:43 -0400

    Route clear before possible caching

--- root/etc/cont-init.d/25-route-cache.sh
+++ root/etc/cont-init.d/25-route-cache.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/with-contenv bash
 
+php /var/www/artisan route:clear >> /var/www/storage/logs/laravel.log 2>&1 || true
+
 if [ "$STARTUP_ROUTE_CACHE" -eq "1" ]; then
 php /var/www/artisan route:cache >> /var/www/storage/logs/laravel.log 2>&1 || true
 echo "Done route:cache"
